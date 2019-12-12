518855e79b10845c0f661df2ce43cf9dfaf157a4
Bùi Đăng Khoa <khoadang200598@gmail.com>
2019-12-12 15:03:12 +0700

    khoa cap nhap detailProduct

---
+++ CommerceApp/CommerceApp/ViewModels/TestItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CommerceApp.Models;
+
+namespace CommerceApp.ViewModels
+{
+    public class TestItemViewModel
+    {
+        public TestItem Items { get; set; }
+        public TestItemViewModel()
+        {
+            Items = new TestItem { Title = "Title", Detail = "Detail" };
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Xamarin-Ecommerce-App
+Develop a Ecommerce App by Xamarin
