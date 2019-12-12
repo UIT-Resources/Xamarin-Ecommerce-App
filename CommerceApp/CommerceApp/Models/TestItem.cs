0c561fbf74413a2b22b24063ef08582f25b991e0
UIT-Resources <16521787@gm.uit.edu.vn>
2019-12-12 12:10:15 +0700

    Initialize MVVM Model

---
+++ CommerceApp/CommerceApp/Models/TestItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CommerceApp.Models
+{
+    public class TestItem:BindableBase
+    {
+        string title;
+        string detail;
+        public string Title
+        {
+            get { return title;  }
+            set
+            {
+                if (!value.Equals(title,StringComparison.Ordinal))
+                {
+                    title = value;
+                    OnPropertyChanged("Title");
+                };
+            }
+        }
+        public string Detail 
+        {
+            get 
+            {
+                return detail;
+            }
+            set
+            {
+                if(!value.Equals(detail, StringComparison.Ordinal))
+                {
+                    detail = value;
+                    OnPropertyChanged("Detail");
+                }
+            }
+        }
+    }
+}
---
+++ CommerceApp/CommerceApp/Views/TestItemView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using CommerceApp.ViewModels;
+
+namespace CommerceApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestItemView : ContentPage
+    {
+        public TestItemView()
+        {
+            InitializeComponent();
+            this.BindingContext = new TestItemViewModel();
+        }
+    }
+}
