00e2b1bff95a7065a0bb1f6419b2040948764f97
UIT-Resources <16521787@gm.uit.edu.vn>
2019-12-26 11:52:58 +0700

    Fix Error : Add PropertyChanged Feature

--- CommerceApp/CommerceApp/Models/User.cs
+++ CommerceApp/CommerceApp/Models/User.cs
@@ -5,22 +5,37 @@ using SQLite;
 
 namespace CommerceApp.Models
 {
-    public class User : IObject
+    public class User : BindableBase, IObject
     {
-        [PrimaryKey, AutoIncrement]
-        public int ID { get; set; }
-        [MaxLength(50)]
-        public string UserName { get; set; }
-        [MaxLength(50)]
-        public string PassWord { get; set; }
-        [MaxLength(50)]
-        public string FullName { get; set; }
-        public DateTime BirthDay { get; set; }
-        public int PhoneNumber { get; set; }
-        [MaxLength(50)]
-        public string Email { get; set; }
-        [MaxLength(5)]
-        public string Sex { get; set; }
+        [Ignore]
+        int id { get; set; }
+        [Ignore]
+        string username { get; set; }
+        [Ignore]
+        string password { get; set; }
+        [Ignore]
+        DateTime birthday { get; set; }
+        [Ignore]
+        int phonenumber { get; set; }
+        [Ignore]
+        string email { get; set; }
+        [Ignore]
+        string sex { get; set; }
+        [Ignore]
+        string iconurl { get; set; }
 
+        [PrimaryKey, AutoIncrement]
+        public int ID { get { return id; } set { id = value; OnPropertyChanged("ID"); } }
+        [MaxLength(50)]
+        public string UserName { get { return username; } set { username = value; OnPropertyChanged("UserName"); } }
+        [MaxLength(50)]
+        public string PassWord { get { return password; } set { password = value; OnPropertyChanged("PassWord"); } }
+        public DateTime BirthDay { get { return birthday; } set { birthday = value; OnPropertyChanged("BirthDay"); } }
+        public int PhoneNumber { get { return phonenumber; } set { phonenumber = value; OnPropertyChanged("PhoneNumber"); } }
+        [MaxLength(50)]
+        public string Email { get { return email; } set { email = value; OnPropertyChanged("Email"); } }
+        [MaxLength(5)]
+        public string Sex { get { return sex; } set { sex = value; OnPropertyChanged("Sex"); } }
+        public string IconUrl { get { return iconurl; } set { iconurl = value; OnPropertyChanged("IconUrl"); } }
     }
 }
