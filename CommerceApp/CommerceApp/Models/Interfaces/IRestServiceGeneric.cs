5b8b24f1c71e0ef1bd2acda5ab1635e9be9816d5
UIT-Resources <16521787@gm.uit.edu.vn>
2019-12-29 13:19:29 +0700

    RestServiceGeneric successfully buit

---
+++ CommerceApp/CommerceApp/Models/Interfaces/IRestServiceGeneric.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommerceApp.Models.Interfaces
+{
+    public interface IRestServiceGeneric
+    {
+			//Get List Items Type T
+			Task<List<T>> RefreshObjectAsync<T>(string ItemsUrl);
+			//Get Item Type T Which has ID = id
+			Task<T> GetObjectAsync<T>(string ItemsUrl, string id);
+			//Save Item Type T. If It's already exist then Update else Create
+			Task SaveObjectAsync<T>(string ItemsUrl, T item, bool isNewItem);
+		    //Delete Item Type T.
+			Task DeleteObjectAsync(string ItemsUrl,string id);
+	}
+}
